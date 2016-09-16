4f5e39698346dd728366755605bae58773c8d634
elkinvg <elkin@jinr.ru>
2016-09-16 15:09:29 +0300

    replace priority to user_type in localStorage

--- app/Application.js
+++ app/Application.js
@@ -125,7 +125,7 @@ Ext.define('ControlRoomDesktop.Application', {
         window.location.reload();
         localStorage.removeItem('login');
         localStorage.removeItem('password');
-        localStorage.removeItem('priority');
+        localStorage.removeItem('user_type');
     },
     //
     //
